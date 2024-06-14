723f76f703f989937506e1ca19f2c13b9a14d5e4
General_K1ng <General_K1ng@outlook.com>
2024-06-14 21:50:34 +0800

    feat(interrupt): Timer External Clock

---
+++ 6.2-Timer External Clock/System/Inc/Timer.h
@@ -0,0 +1,11 @@
+//
+// Created by General_K1ng on 2024/6/14.
+//
+
+#ifndef __TIMER_H
+#define __TIMER_H
+
+void Timer_Init(void);
+uint16_t Timer_GetCounter(void);
+
+#endif //INC_6_1_TIMERINTERRUPT_TIMER_H
---
+++ 6.2-Timer External Clock/User/Src/main.c
@@ -0,0 +1,38 @@
+//
+// Created by General_K1ng on 2024/4/24.
+//
+#include "OLED.h"
+#include "Delay.h"
+#include "Timer.h"
+#include "stm32f10x.h"
+
+uint16_t Num;
+
+int main(void) {
+    OLED_Init();
+    Timer_Init();
+
+    //OLED_ShowString(1, 3 , "Hello World!");
+    //OLED_ShowString(3, 3 , "General_K1ng");
+    OLED_ShowString(2, 1, "Num:");
+    OLED_ShowString(3, 1, "CNT:");
+    while (1) {
+
+        OLED_ShowNum(2, 6, Num, 5);
+        OLED_ShowNum(3, 6, Timer_GetCounter(), 5);
+    }
+}
+/**
+ * TIM2中断服务函数
+ * 该函数在TIM2中断发生时被调用，用于处理定时器2的更新中断。
+ * 主要功能是增加一个计数器，并清除中断标志位。
+ */
+void TIM2_IRQHandler(void) {
+    // 检查TIM2的更新中断是否发生
+    if (TIM_GetITStatus(TIM2, TIM_IT_Update) == SET) {
+        // 中断发生时，计数器加一
+        Num++;
+        // 清除中断标志位，防止中断再次触发
+        TIM_ClearITPendingBit(TIM2, TIM_IT_Update);
+    }
+}
