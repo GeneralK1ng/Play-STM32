d11aa8f30f3d8da6fb8d7d3046c6266d0eca93da
General_K1ng <General_K1ng@outlook.com>
2024-06-16 17:17:53 +0800

    feat(PWMI): PWMI Measurement Freq Duty cycle

---
+++ 6.7-PWMI Measurement Frequency Duty Cycle/User/Src/main.c
@@ -0,0 +1,24 @@
+//
+// Created by General_K1ng on 2024/4/24.
+//
+#include "OLED.h"
+#include "PWM.h"
+#include "IC.h"
+
+int main(void) {
+
+    OLED_Init();
+    PWM_Init();
+    IC_Init();
+
+    OLED_ShowString(2, 1, "Freq: 00000Hz");
+    OLED_ShowString(3, 1, "Duty: 00%");
+
+    PWM_SetPrescaler(7200 - 1);
+    PWM_SetCompare1(80);
+
+    while (1) {
+        OLED_ShowNum(2, 7, IC_GetFreq(), 5);
+        OLED_ShowNum(3, 7, IC_GetDuty(), 2);
+    }
+}
---
+++ 6.7-PWMI Measurement Frequency Duty Cycle/hardware/Inc/IC.h
@@ -0,0 +1,14 @@
+//
+// Created by General_K1ng on 2024/6/16.
+//
+
+#ifndef __IC_H
+#define __IC_H
+
+#include <stdint-gcc.h>
+
+void IC_Init(void);
+uint32_t IC_GetFreq(void);
+uint32_t IC_GetDuty(void);
+
+#endif //__IC_H
